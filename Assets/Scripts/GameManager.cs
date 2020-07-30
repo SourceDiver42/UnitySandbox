f63f44ec17e228c3654e10707501001053b75ec2
Mika Beckerich <beckerichmika@gmail.com>
2020-07-30 14:22:48 +0200

    GameManager

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour
+{
+    public GameObject loadingScreen;
+    
+    private void Awake()
+    {
+        SceneManager.LoadSceneAsync((int)SceneIndex.MAIN_MENU, LoadSceneMode.Additive);
+    }
+    
+    public void LoadGame() {
+        loadingScreen.SetActive(true);
+        SceneManager.UnloadSceneAsync((int)SceneIndex.MAIN_MENU);
+        SceneManager.LoadSceneAsync((int)SceneIndex.MAIN_SCENE, LoadSceneMode.Additive);
+    }
+}
